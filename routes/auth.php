127a3fec624d947cc42a1313b8022dd7113d3b1a
unknown <minhn6673@gmail.com>
2026-03-09 22:40:58 +0700

    add composer json

--- routes/auth.php
+++ routes/auth.php
@@ -26,12 +26,12 @@ Route::middleware('auth:sanctum')->group(function () {
     // UC5 - Xem thông tin cá nhân
     Route::get('/profile', [AuthController::class, 'me']);
 
-    // UC6 - Chuyên môn giảng viên
-    Route::get('/expertises', [AuthController::class, 'getExpertises']);//  Lấy danh sách
+    // UC6 - Chuyên môn giảng viên-đang ở routes không phù hợp
+    Route::get('/expertises', [AuthController::class, 'getExpertises']);//  Lấy danh sách | đang 
     Route::put('/lecturer/expertises', [AuthController::class, 'updateExpertise']); // Cập nhật
-    // UC7 - Yêu cầu nghỉ phép dài hạn
+    // UC7 - Yêu cầu nghỉ phép dài hạn-đang ở routes không phù hợp
     Route::post('/lecturer/leave-requests', [AuthController::class, 'createLeaveRequest']);
-    // UC8 - Thông báo
+    // UC8 - Thông báo-đang ở routes không phù hợp
     Route::get('/notifications', [AuthController::class, 'getNotifications']); // Lấy danh sách
     Route::put('/notifications/{id}/read', [AuthController::class, 'markAsRead']);      // Đánh dấu đã xem
 });
