454c84af67a20dad8a4c1a9f6838108ec6c611eb
HelloWorld2k5 <truongyukihara@gmail.com>
2026-03-26 20:59:02 +0700

    update config database for production

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            // Không nên điền url vì sẽ ưu tiên hơn việc config env trên render
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
